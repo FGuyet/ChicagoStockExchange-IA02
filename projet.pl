9a9d8463a63c6ba295be1e9fd6ffef2d035d8650
FGuyet <florian.guyet@etu.utc.fr>
2015-05-15 22:15:29 +0200

    Transformation en prédicats avec arguments
    Transformation de tous les prédicats pour qu'ils aient des arguments.
    
    Dans sujet :
    -arguments avec +  = arguments ayant deja une valeur
    -arguments avec ?= argument dont on veut la valeur
    
    ex de cours : concat(+L1, +L2, ?R) : concatène L1 et L2 dans R

--- projet.pl
+++ projet.pl
@@ -4,9 +4,9 @@ bourse([[ble,7],[riz,6],[cacao,6],[cafe,6],[sucre,6],[mais,6]]).
 
 /*Affichage de la bourse*/
 
-affiche_bourse :- 	write('\nVoici la bourse :\n'),
+affiche_bourse(Bourse) :- 	write('\nVoici la bourse :\n'),
 					write('-----------------\n'),
-					bourse(X) , affiche_valeurs(X),
+					affiche_valeurs(Bourse),
 					write('\n').
 
 affiche_valeurs([]).
@@ -15,28 +15,31 @@ affiche_valeurs([T|Q]):- affiche_valeur(T), affiche_valeurs(Q).
 affiche_valeur([Nom,Valeur]):- write(Nom), write(' :\t'), write(Valeur), write('\n').
 
 
+%----------------------------------------------------------------------
+
 %Marchandises
-marchandises([[maÔs, riz, ble, ble],
-[ble, maÔs, sucre, riz],
-[cafe, sucre, cacao, riz],
-[cafe, maÔs, sucre, maÔs],
-[cacao, maÔs, ble, sucre],
-[riz, cafe, sucre, ble],
-[cafe, ble, sucre, cacao],
-[maÔs, cacao, cacao, cafe],
-[riz,riz,cafe,cacao]]) .
+marchandises(	[	
+				[mais, riz, ble, ble],
+				[ble, mais, sucre, riz],
+				[cafe, sucre, cacao, riz],
+				[cafe, mais, sucre, mais],
+				[cacao, mais, ble, sucre],
+				[riz, cafe, sucre, ble],
+				[cafe, ble, sucre, cacao],
+				[mais, cacao, cacao, cafe],
+				[riz,riz,cafe,cacao]
+				]
+			) .
 
 
 
-/*--------------------
-Affichage des marchandises en imprimant chaque pile
-*/
+%Affichage des marchandises en imprimant chaque pile
 
-affiche_marchandises:- 	write('\nVoici les piles et leur contenu :\n'),
-						write('---------------------------------\n'),
-						marchandises(X), affiche_piles(X),
-						write('\n').
-/* X prends la valeur du parametre dans marchandises ==> puis affiche cette liste de piles gr‚ce aux prÈdicats ci-dessous */ 
+affiche_marchandises(Marchandises):- 	write('\nVoici les piles et leur contenu :\n'),
+										write('---------------------------------\n'),
+										affiche_piles(Marchandises),
+										write('\n').
+/* X prends la valeur du parametre dans marchandises ==> puis affiche cette liste de piles gr√¢ce aux pr√©dicats ci-dessous */ 
 
 %Piles
 affiche_piles([]).
@@ -46,42 +49,45 @@ affiche_piles([T|Q]) :- affiche_pile(T), write('\n'), affiche_piles(Q).
 affiche_pile([]).
 affiche_pile([T|Q]) :- write(T), write(' '), affiche_pile(Q).
 
-/*------------------------------*/
 
 
 
-/*--------------------
-Affichage de la premiËre carte de chaque pile
-*/
 
-affiche_marchandises_top:- 	write('\nVoici les cartes au sommet de chaque pile :\n'),
-							write('-------------------------------------------\n'),
-							write('|'), marchandises(X), affiche_piles_top(X),
-							write('\n').
-/* X prends la valeur du parametre dans marchandises ==> puis affiche cette liste de piles gr‚ce aux prÈdicats ci-dessous */ 
+%Affichage de la premi√®re carte de chaque pile
+
+affiche_marchandises_top(Marchandises):- 	write('\nVoici les cartes au sommet de chaque pile :\n'),
+											write('-------------------------------------------\n'),
+											write('|'), affiche_piles_top(Marchandises),
+											write('\n').
+/* X prends la valeur du parametre dans marchandises ==> puis affiche cette liste de piles gr√¢ce aux pr√©dicats ci-dessous */ 
 
 %Piles
 affiche_piles_top([]):- !.
 affiche_piles_top([T|Q]) :- affiche_pile_top(T), write('\t|'), affiche_piles_top(Q).
 
 %Pile
-affiche_pile_top([]):- write("VIDE").
+affiche_pile_top([]):- write('VIDE').
 affiche_pile_top([T|_]) :- write(T).
 
 
-/*------------------------------*/
 
+
+%------------------------------------------------------
 
 %PositionTrader
-positionTrader(1).
+positionTrader(3).
 
 /*Affichage de la position du trader par rapport aux piles */
-affiche_position:- 		positionTrader(P), Nbre is (P-1),
-						affiche_tab(Nbre), write('   T\n'),
-						write('\nLa lettre T reprÈsente la position du Trader (position='), write(P), write(')\n\n').
+affiche_position(Position):- 		Nbre is (Position-1),
+									affiche_tab(Nbre), write('   T\n'),
+									write('\nLa lettre T repr√©sente la position du Trader (position='), write(Position), write(')\n\n').
 
 affiche_tab(0):- !.
 affiche_tab(N):- write('\t'), Nbre is (N-1), affiche_tab(Nbre).
+
+
+
+%---------------------------------------------------------
 
 %ReserveJoueur1
 reserveJoueur1([]).
@@ -89,27 +95,34 @@ reserveJoueur1([]).
 %ReserveJoueur2
 reserveJoueur2([]).
 
+
+/* L'affichage des r√©serves	des joueurs*/	
+affiche_joueur1(ReserveJ1) :-	write('\nVoici la r√©serve du Joueur1 :\n'),
+				   				write('-----------------------------\n'),
+				   				affiche_pile(ReserveJ1), write('\n\n').
+
+affiche_joueur2(ReserveJ2) :- 	write('\nVoici la r√©serve du Joueur2 :\n'),
+							  	write('-----------------------------\n'),
+							  	affiche_pile(ReserveJ2), write('\n\n').
+
+
+%----------------------------------------------
+
+
 %Plateau ([Marchandises, Bourse, PositionTrader, ReserveJoueur1, ReserveJoueur2])
-affiche_plateau:- affiche_bourse, affiche_marchandises_top, affiche_position, affiche_joueur1, affiche_joueur2.
-
-
-/* L'affichage des rÈserves	des joueurs*/	
-affiche_joueur1 :- write('\nVoici la rÈserve du Joueur1 :\n'),
-				   write('-----------------------------\n'),
-				   reserveJoueur1(X), affiche_pile(X), write('\n\n').
-
-affiche_joueur2 :- write('\nVoici la rÈserve du Joueur2 :\n'),
-				   write('-----------------------------\n'),
-				   reserveJoueur2(X), affiche_pile(X), write('\n\n').
-/*
-
-%PlateauDepart
-plateau(append(bourse,marchandises, positionTrader, reserveJoueur1,reserveJoueur2)).
-affiche_plateau :- plateau(X) , print(X).
-
-*/
+affiche_plateau([Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2]):-	affiche_bourse(Bourse),
+																				affiche_marchandises_top(Marchandises), 
+																				affiche_position(PositionTrader),
+																				affiche_joueur1(ReserveJ1),
+																				affiche_joueur2(ReserveJ2).
 
 
 
+plateau_depart([Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2]):-	bourse(Bourse),
+																				marchandises(Marchandises), 
+																				positionTrader(PositionTrader),
+																				reserveJoueur1(ReserveJ1),
+																				reserveJoueur2(ReserveJ2).
 
-			 
+
+
