fe0d59f2290b1c315d65d3e983ac2cd698186fe7
FGuyet <florian.guyet@etu.utc.fr>
2015-05-18 21:33:30 +0200

    Scéance de lundi
    - Coup possible
    - jouer-coup : tout fait sauf Marchandise

--- projet.pl
+++ projet.pl
@@ -74,8 +74,6 @@ affiche_pile_top([T|_]) :- write(T).
 
 %------------------------------------------------------
 
-%PositionTrader
-positionTrader(3).
 
 /*Affichage de la position du trader par rapport aux piles */
 affiche_position(Position):- 		Nbre is (Position-1),
@@ -126,3 +124,128 @@ plateau_depart([Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2]):-	b
 
 
 
+
+%------------------------------------------------------------------
+
+coup_possible(	[Marchandises, _, PositionTrader, _,_],
+				[_,Deplacement,Garder,Jeter]):- 
+							length(Marchandises, LongueurM),
+							NewPosition is (PositionTrader+Deplacement) mod LongueurM,
+							recuperer_droite(NewPosition, Marchandises,LongueurM,ProduitDroite),
+							recuperer_gauche(NewPosition, Marchandises, LongueurM, ProduitGauche), 
+							ProduitGauche == Garder, 
+							ProduitDroite == Jeter.
+
+coup_possible(	[Marchandises, _, PositionTrader, _,_],
+				[_,Deplacement,Garder,Jeter]):- 
+							length(Marchandises, LongueurM),
+							NewPosition is (PositionTrader+Deplacement) mod LongueurM,
+							recuperer_droite(NewPosition, Marchandises, LongueurM,ProduitDroite),
+							recuperer_gauche(NewPosition, Marchandises, LongueurM, ProduitGauche), 
+							ProduitGauche == Jeter, 
+							ProduitDroite == Garder.
+							
+						
+recuperer_droite(NewPosition, Marchandises, LongueurM, ProduitDroite):- PositionDroite is (NewPosition + 1) mod LongueurM, 
+																		nth(PositionDroite, Marchandises, PileDroite), 
+																		nth(1, PileDroite, ProduitDroite).
+
+recuperer_gauche(NewPosition, Marchandises, LongueurM, ProduitGauche):- PositionGauche is (NewPosition - 1) mod LongueurM, 
+																		nth(PositionGauche, Marchandises, PileGauche),
+																		nth(1, PileGauche, ProduitGauche).
+
+
+/*
+jouer_coup(	[Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2],
+			[Joueur,Deplacement,Garder,Jeter],
+			[NewMarchandises, NewBourse, NewPositionTrader, NewReserveJ1, NewReserveJ2]) :-
+						coup_possible(	[Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2],
+										[Joueur,Deplacement,Garder,Jeter]),
+						write('Le coup est possible\n'),
+						length(Marchandises,LongueurM),
+						changer_position(PositionTrader, LongueurM, Deplacement, NewPosition),
+						changer_bourse(Bourse,Jeter,NewBourse), 
+						ajouter_reserve(Joueur, ReserveJ1, ReserveJ2).
+*/
+
+jouer_coup(Plateau, Coup, NewPlateau) :-	\+coup_possible(Plateau,Coup),
+											write('ATTENTION : Le coup n\'est pas possible \n'),
+											NewPlateau is Plateau.
+
+changer_position(OldPosition, LongueurM, Deplacement, NewPosition) :- NewPosition is (OldPosition+Deplacement) mod LongueurM.
+
+
+% Changer Bourse
+changer_bourse(Bourse,ble,NewBourse):- 	nth(1,Bourse, Ble),
+										nth(2, Ble, ValeurBle),
+										NewValeurBle is ValeurBle-1,
+										NewBle =[ble, NewValeurBle],
+										nth(2,Bourse, Riz),
+										nth(3,Bourse, Cacao),
+										nth(4,Bourse, Cafe),
+										nth(5,Bourse, Sucre),
+										nth(6,Bourse, Mais),
+										NewBourse = [NewBle, Riz, Cacao, Cafe, Sucre, Mais],!.
+
+changer_bourse(Bourse,riz,NewBourse):- 	nth(1,Bourse, Ble),
+										nth(2,Bourse, Riz),
+										nth(2, Riz, ValeurRiz),
+										NewValeurRiz is ValeurRiz-1,
+										NewRiz =[riz, NewValeurRiz],
+										nth(3,Bourse, Cacao),
+										nth(4,Bourse, Cafe),
+										nth(5,Bourse, Sucre),
+										nth(6,Bourse, Mais),
+										NewBourse = [Ble, NewRiz, Cacao, Cafe, Sucre, Mais],!.
+
+changer_bourse(Bourse,cacao,NewBourse):- nth(1,Bourse, Ble),
+										nth(2,Bourse, Riz),
+										nth(3,Bourse, Cacao),
+										nth(2, Cacao, ValeurCacao),
+										NewValeurCacao is ValeurCacao-1,
+										NewCacao =[cacao, NewValeurCacao],
+										nth(4,Bourse, Cafe),
+										nth(5,Bourse, Sucre),
+										nth(6,Bourse, Mais),
+										NewBourse = [Ble, Riz, NewCacao, Cafe, Sucre, Mais],!.
+
+changer_bourse(Bourse,cafe,NewBourse):- nth(1,Bourse, Ble),
+										nth(2,Bourse, Riz),
+										nth(3,Bourse, Cacao),
+										nth(4,Bourse, Cafe),
+										nth(2, Cafe, ValeurCafe),
+										NewValeurCafe is ValeurCafe-1,
+										NewCafe =[cafe, NewValeurCafe],
+										nth(5,Bourse, Sucre),
+										nth(6,Bourse, Mais),
+										NewBourse = [Ble, Riz, Cacao, NewCafe, Sucre, Mais],!.
+
+changer_bourse(Bourse,sucre,NewBourse):- nth(1,Bourse, Ble),
+										nth(2,Bourse, Riz),
+										nth(3,Bourse, Cacao),
+										nth(4,Bourse, Cafe),
+										nth(5,Bourse, Sucre),
+										nth(2, Sucre, ValeurSucre),
+										NewValeurSucre is ValeurSucre-1,
+										NewSucre =[sucre, NewValeurSucre],
+										nth(6,Bourse, Mais),
+										NewBourse = [Ble, Riz, Cacao, Cafe, NewSucre, Mais],!.
+
+changer_bourse(Bourse,mais,NewBourse):- nth(1,Bourse, Ble),
+										nth(2,Bourse, Riz),
+										nth(3,Bourse, Cacao),
+										nth(4,Bourse, Cafe),
+										nth(5,Bourse, Sucre),
+										nth(6,Bourse, Mais),
+										nth(2, Mais, ValeurMais),
+										NewValeurMais is ValeurMais-1,
+										NewMais =[mais, NewValeurMais],
+										NewBourse = [Ble, Riz, Cacao, Cafe, Sucre, NewMais],!.
+
+
+ajouter_reserve(j1,Garder , ReserveJ1, _, NewReserveJ1,_):- 	IntReserveJ1 = ReserveJ1,
+																append(IntReserveJ1,[Garder], NewReserveJ1).
+
+
+ajouter_reserve(j2,Garder , _, ReserveJ2, _, NewReserveJ2):-	IntReserveJ2 = ReserveJ2,
+																append(IntReserveJ2,[Garder], NewReserveJ2).
