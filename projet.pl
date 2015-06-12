ced32481fbfab216761df8b62d58c1d826fa3ec6
FGuyet <florian.guyet@etu.utc.fr>
2015-06-12 16:32:43 +0200

    scores_coups_possibles

--- projet.pl
+++ projet.pl
@@ -33,7 +33,10 @@ affiche_plateau([Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2]):-	
 																				affiche_marchandises_top(Marchandises), 
 																				affiche_position(PositionTrader),
 																				affiche_joueur1(ReserveJ1),
-																				affiche_joueur2(ReserveJ2).
+																				affiche_score(j1,[Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2]),
+																				affiche_joueur2(ReserveJ2),
+																				affiche_score(j2,[Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2]).
+
 marchandises_depart(NewMarchandises) :- liste_marchandises_dispo(Liste), 
 										melanger_liste(Liste, [], Marchandises), 
 										creer_sous_liste(Marchandises, [], NewMarchandises).
@@ -122,10 +125,8 @@ changer_marchandises(Marchandises, NewPosition, NewMarchandises) :- length(March
 																	position_gauche(NewPosition, LongueurM, PositionPileG),
 																	nth(PositionPileG, Marchandises, RecupGauche),
 																	supprimer_tete_liste(RecupGauche, NewRecupGauche),
-																	write('Nouvelle pile gauche : '), affiche_pile(NewRecupGauche), nl,
 																	nth(PositionPileD, Marchandises, RecupDroite),
 																	supprimer_tete_liste(RecupDroite, NewRecupDroite),
-																	write('Nouvelle pile droite : '), affiche_pile(NewRecupDroite),nl,																				
 																	construct_marchandises(Marchandises, NewRecupGauche, NewRecupDroite, NewPosition, NewMarchandises).
 					
 
@@ -369,11 +370,11 @@ reserveJoueur2([]).
 
 affiche_joueur1(ReserveJ1) :-	write('\nVoici la reserve du Joueur1 :\n'),
 				   				write('-----------------------------\n'),
-				   				affiche_pile(ReserveJ1), nl, nl.
+				   				affiche_pile(ReserveJ1), nl.
 
 affiche_joueur2(ReserveJ2) :- 	write('\nVoici la reserve du Joueur2 :\n'),
 							  	write('-----------------------------\n'),
-							  	affiche_pile(ReserveJ2), nl, nl.
+							  	affiche_pile(ReserveJ2), nl.
 
 
 /* Ajout d'un produit à la réserve d'un joueur */
@@ -397,7 +398,7 @@ ajouter_reserve(j2,Garder, ReserveJ1, ReserveJ2, NewReserveJ1 , NewReserveJ2):-	
 
 demander_coup([Joueur,Deplacement,Garder,Jeter], [Marchandises, _, PositionTrader, _, _]) :-		
 													joueurEnCours(Joueur), /* demander_joueur(Joueur), */
-													write('C\'est au tour de '), write(Joueur), write(' de jouer !'), nl,
+													write('\nC\'est au tour de '), write(Joueur), write(' de jouer !'), nl,
 													demander_deplacement(Deplacement), 
 													length(Marchandises, LongueurM),
 													changer_position(PositionTrader, Deplacement, LongueurM, NewPosition),
@@ -533,6 +534,15 @@ jouer_coup(	[Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2],
 						ajouter_reserve(Joueur, Garder, ReserveJ1, ReserveJ2, NewReserveJ1, NewReserveJ2), 
 						changer_marchandises(Marchandises, NewPositionTrader,NewMarchandises),!.
 
+jouer_coup_machine(	[Marchandises, Bourse, PositionTrader, ReserveJ1, ReserveJ2],
+			[Joueur,Deplacement,Garder,Jeter],
+			[NewMarchandises, NewBourse, NewPositionTrader, NewReserveJ1, NewReserveJ2]) :-
+						length(Marchandises,LongueurM),
+						changer_position(PositionTrader, Deplacement, LongueurM, NewPositionTrader),
+						changer_bourse(Bourse,Jeter,NewBourse), 
+						ajouter_reserve(Joueur, Garder, ReserveJ1, ReserveJ2, NewReserveJ1, NewReserveJ2), 
+						changer_marchandises(Marchandises, NewPositionTrader,NewMarchandises),!.
+
 
 /*----------------*/
 /* CHANGER JOUEUR */
@@ -556,27 +566,61 @@ changer_joueur:- retract(joueurEnCours(j2)), asserta(joueurEnCours(j1)),!.
 /* COUP POSSIBLES */
 /*----------------*/
 
+/*retourne tous les coups possibles*/
+
 coups_possibles(Plateau,ListeCoupsPossibles) :- coups_par_deplacement(Plateau, 1, CoupsEn1), 
 												coups_par_deplacement(Plateau, 2, CoupsEn2),
 												coups_par_deplacement(Plateau, 3, CoupsEn3),
 												append(CoupsEn1, CoupsEn2, CoupsEn12),
 												append(CoupsEn12,CoupsEn3, ListeCoupsPossibles).
 
+/*retourne les deux coups possibles par rapport au deplacement*/
 
 coups_par_deplacement([Marchandises,_, PositionTrader, _, _], Deplacement, CoupsDeplacement):-
 							length(Marchandises, LongueurM),
-							recuperer_gauche(PositionTrader, Marchandises, LongueurM, ProduitGauche),
-							recuperer_droite(PositionTrader, Marchandises, LongueurM, ProduitDroite),
+							NewPositionTrader is (PositionTrader + Deplacement),
+							recuperer_gauche(NewPositionTrader, Marchandises, LongueurM, ProduitGauche),
+							recuperer_droite(NewPositionTrader, Marchandises, LongueurM, ProduitDroite),
 							Coup1 = [_, Deplacement,ProduitGauche, ProduitDroite],
 							Coup2 = [_, Deplacement,ProduitDroite, ProduitGauche],
-							append(Coup1,Coup2, CoupsDeplacement).
-
-
-
-deplacements_regles([1,2,3]).
+							append([Coup1],[Coup2], CoupsDeplacement).
 
 produits_regles([mais, ble, cacao, cafe, sucre, riz]).
 
 
+/*---------------*/
+/* MEILLEUR COUP */
+/*---------------*/
 
 
+/*calcul du score d'un joueur*/
+
+calcul_score_joueur(j1,[_,Bourse,_,R1,_], Score):- score_reserve(Bourse, R1, Score),!.
+calcul_score_joueur(j2,[_,Bourse,_,_,R2], Score):- score_reserve(Bourse, R2, Score).
+
+/*calcul du score d'une reserve*/
+
+score_reserve(Bourse, [], 0):-!.
+score_reserve(Bourse, [T|Q], ScoreReserve):- 	score_produit(Bourse, T, ScoreProduit),
+ 												score_reserve(Bourse, Q, ScoreQueue),
+												ScoreReserve is (ScoreProduit + ScoreQueue). 
+/*calcul du score d'un produit*/
+
+score_produit([[Produit, Valeur]|_], Produit, Valeur):-!.
+score_produit([_|Q], Produit, ScoreProduit):- score_produit(Q,Produit, ScoreProduit).
+
+/*Affichage du score d'un joueur*/
+
+affiche_score(Joueur,Plateau):- calcul_score_joueur(Joueur, Plateau, Score),
+								write('Score = '), write(Score), nl.
+
+
+
+scores_coups_possibles(Plateau, [], []):-!.
+scores_coups_possibles(Plateau,[[Joueur|QueueCoup]|Q], ListeCoupsPossiblesScores):-
+				jouer_coup_machine(Plateau,[Joueur|QueueCoup],NewPlateau),
+				calcul_score_joueur(Joueur,NewPlateau, Score),
+				append([[Joueur|QueueCoup]], [Score], ScoreCoup),
+				scores_coups_possibles(Plateau,Q, ListeCoupsPossiblesScoresQueue),
+				append(ListeCoupsPossiblesScoresQueue, [ScoreCoup] ,ListeCoupsPossiblesScores).
+						
